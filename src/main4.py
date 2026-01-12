9edd0709edc357ab7a95add15a14198f5a8ded5c
Thienhd68 <thienhuynh.07072020@gmail.com>
2026-01-12 23:25:25 +0700

    Run test

--- src/main4.py
+++ src/main4.py
@@ -17,7 +17,7 @@ cv2.setMouseCallback('RGB', RGB)
 
 model = YOLO("../model/best.pt")
 
-cap = cv2.VideoCapture('../data/videos/cow2.mp4')
+cap = cv2.VideoCapture('../data/videos/cow5.mp4')
 my_file = open("coco1.txt", "r")
 data = my_file.read()
 class_list = data.split("\n")
